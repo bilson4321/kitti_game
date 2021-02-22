14c8c6e9ce709f945c91ff3e3a856c9beebe7004
ashmikattel <ashmikattel@gmail.com>
2021-02-22 17:44:40 +0545

    game start screen and result screen added

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndex + 1);
+    }
+
+    public void JumpToStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
